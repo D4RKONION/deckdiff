7cb20bebd5ea8744db0acfeb29ad59f48307e5ee
Paul - <k.rafftry@gmail.com>
2018-03-01 19:56:14 +0000

    Ver 0.0.2 Fixed bugs, added support for mtgtapped.net URL inport

--- js/index.js
+++ js/index.js
@@ -1,17 +1,67 @@
+var tappedOutFetcher = function(url, deckToChange) {
+	deckToChange = deckToChange
+	var deckKey = url.slice(url.indexOf("/mtg-decks") + 11);
+	if (deckKey.slice(-1) === "/") {
+		deckKey = deckKey.slice(0, -1);
+	}
+	fetch('https://tappedout.net/api/deck/widget/?deck=' + deckKey)
+		.then(function(response) {
+		return response.json()
+	})
+	.then(function(myJson) {
+		var allButBasics = myJson.cards.split("||");
+		var basicLands = {"Island": "island", "Forest": "forest", "Swamp": "swamp", "Plains": "plains", "Mountain": "mountain"};
+		for (var land in basicLands) {
+			var generalArea = myJson.board.indexOf("boardContainer-" + basicLands[land] + "\">");
+			var generalAreaSliced = myJson.board.slice(generalArea, generalArea + 100);
+			var specificArea = generalAreaSliced.indexOf("x");
+			var numberOfLands = Number(generalAreaSliced.slice(specificArea - 4, specificArea))
+			if (numberOfLands !== 0) {
+				allButBasics.push(numberOfLands + " " + land)
+			}
+		}
+		return allButBasics
+
+	})
+	.then(function(tappedOutDeckList) {
+		
+		if (deckToChange === "deckOne") {
+			document.getElementById("deckOneTextArea").innerHTML = "";
+			for (var cardEnt in tappedOutDeckList) {
+				document.getElementById("deckOneTextArea").innerHTML += (tappedOutDeckList[cardEnt] + "\n")
+			}
+			var deckOneBetaArray = document.getElementById('deckOneTextArea').value.split("\n");
+			deckOneObj = deckObjectMaker(deckOneBetaArray);
+			differnceFinder(deckOneObj, deckTwoObj)
+		} else if (deckToChange === "deckTwo") {
+			document.getElementById("deckTwoTextArea").innerHTML = "";
+			for (var cardEnt in tappedOutDeckList) {
+				document.getElementById("deckTwoTextArea").innerHTML += (tappedOutDeckList[cardEnt] + "\n")
+			}
+			var deckTwoBetaArray = document.getElementById('deckTwoTextArea').value.split("\n");
+			deckTwoObj = deckObjectMaker(deckTwoBetaArray);
+			differnceFinder(deckOneObj, deckTwoObj)
+		}
+		
+		
+	});
+}
+
+
 var deckObjectMaker = function(deckBetaArray) {
-	var mainboardFinished = false;
-	var deckObj = {"mainboard": {}, "sideboard": {}};
+	var maindeckFinished = false;
+	var deckObj = {"maindeck": {}, "sideboard": {}};
 
 	for (var card in deckBetaArray) {
 		var numberInDeck = deckBetaArray[card].slice(0, 1);
 		var cardName = deckBetaArray[card].slice(2);
-	    if (!mainboardFinished) {
+	    if (!maindeckFinished) {
 	        if (deckBetaArray[card] === "") {
-	            mainboardFinished = true;
+	            maindeckFinished = true;
 	        } else {
-	            deckObj["mainboard"][cardName] = numberInDeck;
+	            deckObj["maindeck"][cardName] = numberInDeck;
 	        }
-	    } else if (mainboardFinished) {
+	    } else if (maindeckFinished) {
 			deckObj["sideboard"][cardName] = numberInDeck;
 	    }
 	}
@@ -19,26 +69,26 @@ var deckObjectMaker = function(deckBetaArray) {
 }
 
 var differnceFinder = function(deckOneObj, deckTwoObj) {
-	var textDifference = "<h3>Mainboard</h3>";
+	var textDifference = "<h3>Main Deck</h3>";
 
 	mainDifferenceList = [];
 	mainDealtWithCards = [];
 	
 
-	for (var deckOneCardName in deckOneObj["mainboard"]) {
-		for (var deckTwoCardName in deckTwoObj["mainboard"]) {
+	for (var deckOneCardName in deckOneObj["maindeck"]) {
+		for (var deckTwoCardName in deckTwoObj["maindeck"]) {
 			if (deckOneCardName === deckTwoCardName) {
-				if (deckOneObj["mainboard"][deckOneCardName] !== deckTwoObj["mainboard"][deckTwoCardName]) {
-					mainDifferenceList.push((deckTwoObj["mainboard"][deckTwoCardName] - deckOneObj["mainboard"][deckOneCardName]) + " " + deckOneCardName)
+				if (deckOneObj["maindeck"][deckOneCardName] !== deckTwoObj["maindeck"][deckTwoCardName]) {
+					mainDifferenceList.push((deckTwoObj["maindeck"][deckTwoCardName] - deckOneObj["maindeck"][deckOneCardName]) + " " + deckOneCardName)
 				}
 				mainDealtWithCards.push(deckOneCardName);
 			} else if (deckOneCardName !== deckTwoCardName) {
-				if (typeof deckTwoObj["mainboard"][deckOneCardName] === "undefined" && !mainDealtWithCards.includes(deckOneCardName)) {
-					mainDifferenceList.push((deckOneObj["mainboard"][deckOneCardName] * -1) + " " + deckOneCardName);
+				if (typeof deckTwoObj["maindeck"][deckOneCardName] === "undefined" && !mainDealtWithCards.includes(deckOneCardName)) {
+					mainDifferenceList.push((deckOneObj["maindeck"][deckOneCardName] * -1) + " " + deckOneCardName);
 					mainDealtWithCards.push(deckOneCardName);
 				}
-				if (typeof deckOneObj["mainboard"][deckTwoCardName] === "undefined" && !mainDealtWithCards.includes(deckTwoCardName)) {
-					mainDifferenceList.push(deckTwoObj["mainboard"][deckTwoCardName] + " " + deckTwoCardName);
+				if (typeof deckOneObj["maindeck"][deckTwoCardName] === "undefined" && !mainDealtWithCards.includes(deckTwoCardName)) {
+					mainDifferenceList.push(deckTwoObj["maindeck"][deckTwoCardName] + " " + deckTwoCardName);
 					mainDealtWithCards.push(deckTwoCardName);
 				}
 			}
@@ -60,7 +110,6 @@ var differnceFinder = function(deckOneObj, deckTwoObj) {
 	for (var deckOneCardName in deckOneObj["sideboard"]) {
 		for (var deckTwoCardName in deckTwoObj["sideboard"]) {
 			if (deckOneCardName === deckTwoCardName) {
-				// console.log("name match!: " + deckOneCardName)
 				if (deckOneObj["sideboard"][deckOneCardName] !== deckTwoObj["sideboard"][deckTwoCardName]) {
 					sideDifferenceList.push((deckTwoObj["sideboard"][deckTwoCardName] - deckOneObj["sideboard"][deckOneCardName]) + " " + deckOneCardName)
 				}
